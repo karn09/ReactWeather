a718800e61d7717d87398043f4b41163d4b44f5e
John Nieves <karn09@gmail.com>
2016-09-17 16:37:01 -0400

    connect to weather api and retun temp

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
-var WeatherForm = require('./WeatherForm');
-var WeatherMessage = require('./WeatherMessage');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
   getInitialState: function() {
@@ -10,10 +11,20 @@ var Weather = React.createClass({
     }
   },
   handleSearch: function(location) {
-    this.setState({
-      location: location,
-      temp: 23
-    })
+
+    openWeatherMap.getTemp(location).then( (temp) => {
+      this.setState({
+        location: location,
+        temp: temp
+      });
+    }, (errorMessage) => {
+      alert(errorMessage)
+    });
+    
+    // this.setState({
+    //   location: location,
+    //   temp: 23
+    // })
   },
   render: function() {
     var {temp, location} = this.state;
